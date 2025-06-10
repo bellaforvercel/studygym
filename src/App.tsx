106473b553fbfce166bf0e26a2ae9529d4e43c66
bellaforvercel <abeldesalegn2121@gmail.com>
2025-06-10 11:23:15 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ function App() {
           <Router>
             <Routes>
               <Route path="/study" element={<StudyInterface />} />
-              <Route path="/" element={<Navigate to="/study" replace />} />
-              <Route path="*" element={<Navigate to="/study" replace />} />
+              <Route path="/" element={<Navigate to="/study\" replace />} />
+              <Route path="*" element={<Navigate to="/study\" replace />} />
             </Routes>
             <Toaster />
           </Router>
